d4d36f4e2fc4c772eb934d30ba11fbb1ad544ff6
Rizky Tri Sulistyo <rizkytrisulistyo@gmail.com>
2020-11-15 23:17:17 +0700

    add Intro

---
+++ lib/Screens/Intro/intro_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:penghitung_score/Screens/Intro/components/body.dart';
+
+class IntroScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Body(),
+    );
+  }
+}
